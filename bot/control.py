d10443b64564a9f9932b390af45f23b779d6c624
SRaswan <shauryaraswan@gmail.com>
2024-05-28 14:36:34 -0700

    lol

--- bot/control.py
+++ bot/control.py
@@ -29,10 +29,8 @@ class SampleControllerAsync(Node):
         self.req = GoPupper.Request()
         self.req.command = move_commands[idx]
         self.future = self.cli.call_async(self.req)
-        # rclpy.spin_until_future_complete(self, self.future)
-        while rclpy.ok() and not self.future.done():
-            rclpy.spin_once(self)
-            time.sleep(0.1) 
+        # rclpy.spin_until_future_complete(self, self.future, 0.5)
+        rclpy.spin_once(self, self.future)
         return self.future.result()
 
     def get_user_input(self):
